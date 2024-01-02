356eefc714e7d609412a8712b4fe627e7de0900b
mh.saadatfar <mh.saadatfar@gmail.com>
2024-01-02 00:10:27 -0800

    Use chacha20-poly1352 for authentication code.

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,64 @@ use chacha20::ChaCha20;
 use chacha20::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
 use rand::Rng;
 use hex_literal::hex;
-use chacha20::Key;
-use chacha20::Nonce;
+// use chacha20::Key;
+// use chacha20::Nonce;
+
+use chacha20poly1305::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    ChaCha20Poly1305, Nonce, Key
+};
 
 fn main() {
     println!("Hello, world!");
 
     let plaintext = "Hello";
 
-    let mut rng = rand::thread_rng();
-    let key = rng.gen::<[u8; 32]>();  // 32 bytes key
-    let nonce = rng.gen::<[u8; 12]>(); // 12 bytes nonce for ChaCha20
+    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+    let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
 
-    let my_string = "Hello, world!";
-    let bytes = my_string.as_bytes().to_vec();
+    // let mut rng = rand::thread_rng();
+    // let key = rng.gen::<[u8; 32]>();  // 32 bytes key
+    // let nonce = rng.gen::<[u8; 12]>(); // 12 bytes nonce for ChaCha20
+
+    let bytes = plaintext.as_bytes().to_vec();
     println!("{:x?}", &bytes);
 
-    let cyphered = encrypt(&bytes, &key.into(), &nonce.into());
+    let cyphered = encrypt(bytes, &key, &nonce);
     println!("{:x?}", cyphered);
 
-    let re = encrypt(&cyphered, &key.into(), &nonce.into());
+    let re = decrypt(cyphered, &key, &nonce);
     println!("{:x?}", re);
-
-
 }
 
-fn encrypt(data:&Vec<u8>, key:&Key, nonce:&Nonce) -> Vec<u8> {
-    let mut rng = rand::thread_rng();
-    //let nonce = rng.gen::<[u8; 12]>(); // 12 bytes nonce for ChaCha2
+fn encrypt(plain:Vec<u8>, key:&Key, nonce:&Nonce) -> Vec<u8> {
 
-    let mut cipher = ChaCha20::new(&key, &nonce);
-    let mut buffer = data.clone();
-    cipher.apply_keystream(&mut buffer);
+    let mut cipher = ChaCha20Poly1305::new(&key);
+    //let mut buffer = data.clone();
+    let cipher_result = cipher.encrypt(&nonce, plain.as_ref());
 
-    return buffer.to_vec();
+    let value = match cipher_result {
+        Ok(val) => val,
+        Err(e) => {
+            panic!("Error occurred: {:?}", e)
+        }
+    };
+    return  value;
 }
+
+fn decrypt(encrypted:Vec<u8>, key:&Key, nonce:&Nonce) -> Vec<u8> {
+
+    let mut cipher = ChaCha20Poly1305::new(&key);
+    //let mut buffer = data.clone();
+    let plain_result = cipher.decrypt(&nonce, encrypted.as_ref());
+
+    let value = match plain_result {
+        Ok(val) => val,
+        Err(e) => {
+            panic!("Error occurred: {:?}", e)
+        }
+    };
+    return  value;
+}
+
 
