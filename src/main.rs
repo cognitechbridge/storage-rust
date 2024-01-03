61f739d87ef60fac11024d89b02eec71dbb8890b
mh.saadatfar <mh.saadatfar@gmail.com>
2024-01-02 18:50:01 -0800

    Read file

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,19 @@ use chacha20poly1305::{
     ChaCha20Poly1305, Nonce, Key
 };
 
+use std::fs::File;
+use std::io::{self, Read};
+
 fn main() {
     println!("Hello, world!");
 
-    let plaintext = "Hello";
+    let files_bytes= read_file_to_vec("D:\\1.bin");
+    let bytes = match files_bytes {
+        Ok(val) => val,
+        Err(e) => {
+            panic!("Error occurred: {:?}", e)
+        }
+    };
 
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
@@ -27,14 +36,20 @@ fn main() {
     // let key = rng.gen::<[u8; 32]>();  // 32 bytes key
     // let nonce = rng.gen::<[u8; 12]>(); // 12 bytes nonce for ChaCha20
 
-    let bytes = plaintext.as_bytes().to_vec();
-    println!("{:x?}", &bytes);
+    //println!("{:x?}", &bytes);
 
     let cyphered = encrypt(bytes, &key, &nonce);
-    println!("{:x?}", cyphered);
+    //println!("{:x?}", cyphered);
 
-    let re = decrypt(cyphered, &key, &nonce);
-    println!("{:x?}", re);
+    //let re = decrypt(cyphered, &key, &nonce);
+    //println!("{:x?}", re);
+}
+
+fn read_file_to_vec(filename: &str) -> io::Result<Vec<u8>> {
+    let mut file = File::open(filename)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+    Ok(contents)
 }
 
 fn encrypt(plain:Vec<u8>, key:&Key, nonce:&Nonce) -> Vec<u8> {
