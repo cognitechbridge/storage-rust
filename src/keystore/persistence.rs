a77e5bde76b70cf5ee13c3408f2f1eeb44b08b67
mh.saadatfar <mh.saadatfar@gmail.com>
2024-01-16 16:55:15 -0800

    Seperate get_persist_path

--- src/keystore/persistence.rs
+++ src/keystore/persistence.rs
@@ -1,5 +1,6 @@
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Write};
+use std::path::PathBuf;
 use aead::Aead;
 use anyhow::{Result};
 use crypto_common::{KeyInit, KeySizeUser};
@@ -11,21 +12,28 @@ type Key<N> = GenericArray<u8, N>;
 
 
 impl<N: ArrayLength<u8>, C: KeySizeUser<KeySize=N> + KeyInit + Aead> KeyStore<N, C> {
-    pub fn persist_key(&self, key_id: &str, key: &Key<N>) -> Result<()> {
-        let str = self.serialize_key_pair(key_id, key);
+    fn get_persist_path() -> Result<PathBuf> {
         let mut path = get_user_path()?;
         path.push("key_store");
+        Ok(path)
+    }
+    pub fn persist_key(&self, key_id: &str, key: &Key<N>) -> Result<()> {
+        let path = Self::get_persist_path()?;
         let mut file = OpenOptions::new()
             .append(true)
             .create(true)
             .open(path)?;
-        writeln!(file, "{}", str?)?;
+
+        let str = self.serialize_key_pair(key_id, key)?;
+        writeln!(file, "{}", str)?;
         Ok(())
     }
     pub fn load_from_persist(&mut self) -> Result<()> {
-        let mut path = get_user_path()?;
-        path.push("key_store");
-        let mut file = File::open(path).unwrap();
+        let path = Self::get_persist_path()?;
+        if !path.exists() {
+            return Ok(());
+        }
+        let mut file = File::open(path)?;
         let mut buf = vec![];
         file.read_to_end(&mut buf)?;
         let str = String::from_utf8(buf)?;
