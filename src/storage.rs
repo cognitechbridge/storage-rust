9a46df7caeffd11871750cc68f50a35e6fa3b343
mh.saadatfar <mh.saadatfar@gmail.com>
2024-01-21 16:11:02 -0800

    Fix async error.

--- src/storage.rs
+++ src/storage.rs
@@ -1,10 +1,12 @@
+use std::future::Future;
 use std::io::{Read, Write};
+use anyhow::Result;
 pub mod s3;
 
 pub trait StorageUpload {
-    async fn upload<R: Read>(& self, reader: &mut R, key: &str) -> anyhow::Result<()>;
+    fn upload<R: Read>(& self, reader: &mut R, key: &str) -> impl Future<Output = Result<()>>;
 }
 
 pub trait StorageDownload {
-    async fn download<W: Write>(& self, writer: &mut W, key: &str) -> anyhow::Result<usize>;
+    fn download<W: Write>(& self, writer: &mut W, key: &str) -> impl Future<Output = Result<usize>>;
 }
